4a2e4d445d850a457824990e378fe04d1e6c2c01
lun2hang <99711103+lun2hang@users.noreply.github.com>
2024-04-23 16:07:03 +0800

    Update llms_group_evolution.py

--- llms_group_evolution.py
+++ llms_group_evolution.py
@@ -1,4 +1,4 @@
-#modify a trl demo to orgnize a group of llms to generate more positive comments to a movie, a bert based sentiments classifier acts as reward model
+#modify a trl demo to orgnize a group of llms to generate more positive consequent comments to a movie, a bert based sentiments classifier acts as reward model, max reward caped by 3.0
 
 '''
 group evolution fake code:
